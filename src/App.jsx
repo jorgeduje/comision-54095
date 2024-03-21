80bbe8b22e635b6550e13e2a1d73f9c972d0a4a1
jorgeduje <jorgeduje4@gmail.com>
2024-03-21 09:37:38 -0300

    se limpia el main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-
-import Footer from "./Footer.jsx"
-
 function App() {
-
-  let nombre = "pepe"
-
-  const sumar = ()=>{
-    console.log( 5 + 2 )
-  }
-
-  return <div>
-    <h1>hola {nombre}  </h1>
-    <h2>como estas?</h2>
-    <button onClick={sumar}>Sumar</button>
-    <Footer />
-  </div>
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
